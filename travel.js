b6268dd4ec0650a5b66d06fe947a456aaa555858
AlexDrozd <shuradrozd@mail.ru>
2017-10-20 17:22:06 +0300

    Moved 'fortune cookie' functionality into module.

--- travel.js
+++ travel.js
@@ -1,38 +1,40 @@
 var express = require('express');
+var app = express();
+var fortune = require('./lib/fortune.js');
 var handlebars = require('express3-handlebars')
     .create({defaultLayout: 'main'});
+
 app.engine('handlebars', handlebars.engine);
+
+app.use(express.static(__dirname + '/public'));
 app.set('view engine','handlebars');
-var app = express();
 app.set('port', process.env.PORT || 3000);
 // route to root page
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Travel Site');
+    res.render('home');
 });
 
 //route to about page
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Travel Site');
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 //page 404
-app.use(function(req, res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found')
+app.use(function(req, res, next){
+   res.status(404);
+   res.render('404');
 
 });
 
 // page 500
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 
 });
+
+
 app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
