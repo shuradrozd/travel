6ccd57855b3885db06d160fe560820776a32a565
AlexDrozd <shuradrozd@mail.ru>
2018-02-09 18:40:23 +0300

    added eslint nodemon and refactor to es6 format

--- public/qa/test-global.js
+++ public/qa/test-global.js
@@ -1,12 +1,13 @@
-suite('Global Tests', function(){
-    test('page has a valid title', function(){
-        assert(document.title && document.title.match(/\S/) &&
+const assert = require('chai').assert;
+suite('Global Tests', function() {
+  test('page has a valid title', function() {
+    assert(document.title && document.title.match(/\S/) &&
             document.title.toUpperCase() !== 'TODO');
-    });
+  });
 });
 
-suite('"About" Page Tests', function(){
-    test('page should contain link to contact page', function(){
-        assert($('a[href="/contact"]').length);
-    });
+suite('"About" Page Tests', function() {
+  test('page should contain link to contact page', function() {
+    assert($('a[href="/contact"]').length);
+  });
 });
--- travel.js
+++ travel.js
@@ -1,54 +1,50 @@
-var express = require('express');
-var app = express();
-var fortune = require('./lib/fortune.js');
-var url = require('url');
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars')
+  .create({defaultLayout: 'main'});
 
-var handlebars = require('express3-handlebars')
-    .create({defaultLayout: 'main'});
+const getFortune = require('./lib/fortune.js');
 
 app.engine('handlebars', handlebars.engine);
-
-app.use(express.static(__dirname + '/public'));
-app.set('view engine','handlebars');
+app.set('view engine', 'handlebars');
+// work with static content
+app.use(express.static(`${__dirname }/public`));
 app.set('port', process.env.PORT || 3000);
 // route to root page
-app.get('/', function(req, res){
-    res.render('home');
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-//route to about page
-app.get('/about', function(req, res) {
-    res.render('about', {
-        fortune: fortune.getFortune(),
-        pageTestScript: '/qa/tests-about.js'
-    } );
+// route to about page
+app.get('/about', (req, res) => {
+  res.render('about', {
+    fortune: getFortune(),
+    // pageTestScript: '../../public/qa/tests-global.js',
+  } );
 });
 
-//page 404
-app.use(function(req, res, next){
-   res.status(404);
-   res.render('404');
-
+ app.disable('x-powered-by');
+// page 404
+app.use((req, res, next) => {
+  res.status(404).render('404');
 });
 
 // page 500
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-
-});
-
-app.use(function(req,res, next){
-   res.locals.showTests = app.get('env') !== 'production'
-   && req.query.test == '1';
-   next();
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('500');
 });
 
 
-
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
+// add check for test page
+app.use((req, res, next) => {
+  res.locals.showTests = app.get('env') !== 'production'
+   && req.query.test === '1';
+  next();
 });
 
+
+app.listen(app.get('port'), () => {
+  console.log( `Express started on http://localhost:${
+    app.get('port') }; press Ctrl-C to terminate.` );
+});
